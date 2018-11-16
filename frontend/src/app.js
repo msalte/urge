57a4c9e5497ad3b39aa1535b86ce670364ad16ae
Morten <msalte86@outlook.com>
2018-11-16 21:15:25 +0100

    Experimenting with Semantic UI React

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,20 +2,45 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import { Input, Menu } from "semantic-ui-react";
+
 import Home from "./components/pages/home";
 import Profile from "./components/pages/profile";
 
-class Header extends Component {
-  render() {
-    return "HEADER";
-  }
-}
-
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { activeItem: "home" };
+  }
+
+  handleItemClick(name) {
+    this.setState({ activeItem: name });
+  }
+
   render() {
+    const { activeItem } = this.state;
+
     return (
       <React.Fragment>
-        <Header />
+        <Menu pointing>
+          <Menu.Item
+            name="home"
+            active={activeItem === "home"}
+            onClick={() => this.handleItemClick("home")} />
+          <Menu.Item
+            name="messages"
+            active={activeItem === "messages"}
+            onClick={() => this.handleItemClick("messages")} />
+          <Menu.Item
+            name="friends"
+            active={activeItem === "friends"}
+            onClick={() => this.handleItemClick("friends")} />
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Input icon="search" placeholder="Search..." />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/profile" render={props => <Profile {...props} />} />
