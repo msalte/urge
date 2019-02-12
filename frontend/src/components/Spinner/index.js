487040d77a062368a5ce8455e4c76a17e3c12e26
Morten Salte <msalte86@gmail.com>
2019-02-12 20:21:13 +0100

    Removed unused divs in spinner component

--- frontend/src/components/Spinner/index.js
+++ frontend/src/components/Spinner/index.js
@@ -2,17 +2,5 @@ import React from "react";
 import styles from "./styles.scss";
 
 export default () => {
-    return (
-        <div className={styles.ldsDualRing}>
-            <div />
-            <div />
-            <div />
-            <div />
-            <div />
-            <div />
-            <div />
-            <div />
-            <div />
-        </div>
-    );
+    return <div className={styles.ldsDualRing} />;
 };
