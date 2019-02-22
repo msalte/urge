38913f4971e94553e715b214c00fbae8c626f869
Morten Salte <morten.salte@bouvet.no>
2019-02-22 14:54:56 +0100

    Modal improvements

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -3,14 +3,22 @@ import { createPortal } from "react-dom";
 import Button from "../Button";
 import classNames from "classnames";
 import styles from "./styles.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const container = document.getElementById("overlays");
 
-export default ({ isOpen, onClose, children }) => {
+export default ({ title, isOpen, onClose, children }) => {
     return createPortal(
         <div className={classNames(styles.modal, { [styles.open]: isOpen })}>
-            {children}
-            <Button onClick={() => onClose()}>Close</Button>
+            <div className={styles.headerRow}>
+                <div>{title}</div>
+                <div className={styles.button}>
+                    <Button onClick={() => onClose()} stretch>
+                        <FontAwesomeIcon icon="times" />
+                    </Button>
+                </div>
+            </div>
+            <div className={styles.body}>{children}</div>
         </div>,
         container
     );
