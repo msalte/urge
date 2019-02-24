29f6931d57dbdd79bf6591e3d66551100caf760a
Morten Salte <msalte86@outlook.com>
2019-02-24 17:13:44 +0100

    Modal now uses grayed out background. Clicking outside modal will close it.

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { createPortal } from "react-dom";
 import Button from "../Button";
 import classNames from "classnames";
@@ -8,17 +8,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const container = document.getElementById("overlays");
 
 export default ({ title, isOpen, onClose, children }) => {
+    const ref = useRef(null);
+    const handleClickOutside = (ref, e) => {
+        if (e.target.contains(ref.current)) {
+            onClose();
+        }
+    };
+
     return createPortal(
-        <div className={classNames(styles.modal, { [styles.open]: isOpen })}>
-            <div className={styles.headerRow}>
-                <div>{title}</div>
-                <div className={styles.button}>
-                    <Button onClick={() => onClose()} stretch>
-                        <FontAwesomeIcon icon="times" />
-                    </Button>
+        <div
+            className={classNames(styles.modal, { [styles.open]: isOpen })}
+            onClick={e => handleClickOutside(ref, e)}
+        >
+            <div
+                ref={ref}
+                className={classNames(styles.content, {
+                    [styles.open]: isOpen,
+                })}
+            >
+                <div className={styles.headerRow}>
+                    <div>{title}</div>
+                    <div className={styles.button}>
+                        <Button onClick={() => onClose()} stretch>
+                            <FontAwesomeIcon icon="times" />
+                        </Button>
+                    </div>
                 </div>
+                <div className={styles.body}>{children}</div>
             </div>
-            <div className={styles.body}>{children}</div>
         </div>,
         container
     );
