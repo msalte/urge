ad4e7560a4c10a88d95e2498326b14aa8a06693e
Morten Salte <msalte86@outlook.com>
2019-03-24 13:20:39 +0100

    Added design time db factory to rememedy build failure for EF migrations.

---
+++ backend/Urge.Users/Database/DesignTimeUsersContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Urge.Users.Database
+{
+    public class DesignTimeUsersContextFactory : IDesignTimeDbContextFactory<UsersContext>
+    {
+        public UsersContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UsersContext>();
+            optionsBuilder.UseSqlServer("Server=idontexist;Database=neitherdoi;Trusted_Connection=True;");
+            return new UsersContext(optionsBuilder.Options);
+
+        }
+    }
+}
